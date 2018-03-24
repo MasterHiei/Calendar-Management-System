f5750353ee45beaff413de489882dbaaf50a5a11
Hiei <hiei.tmhi@foxmail.com>
2018-03-24 22:24:34 +0900

    修复数据库

--- src/main/resources/DB/init.sql
+++ src/main/resources/DB/init.sql
@@ -6,105 +6,105 @@ INSERT INTO tbl_role (
   role_code, role_name,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (1, 'Admin', 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (1, 'Admin', 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role (
   role_code, role_name,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (2, 'Leader', 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (2, 'Leader', 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role (
   role_code, role_name,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (3, 'Member', 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (3, 'Member', 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 
 -- INIT TABLE AUTH --
 INSERT INTO tbl_auth (
   auth_code, auth_name,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (1, 'Insert', 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (1, 'Insert', 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_auth (
   auth_code, auth_name,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (2, 'Delete', 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (2, 'Delete', 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_auth (
   auth_code, auth_name,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (3, 'Update', 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (3, 'Update', 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_auth (
   auth_code, auth_name,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (4, 'Query', 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (4, 'Query', 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 
 -- INIT TABLE ROLE_AUTH --
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (1, 1, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (1, 1, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (1, 2, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (1, 2, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (1, 3, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (1, 3, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (1, 4, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (1, 4, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 
 
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (2, 1, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (2, 1, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (2, 2, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (2, 2, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (2, 3, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (2, 3, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (2, 4, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (2, 4, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 
 
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (3, 1, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (3, 1, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (3, 2, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (3, 2, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (3, 3, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (3, 3, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_role_auth (
   role_code, auth_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES (3, 4, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (3, 4, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 
 
 -- INIT TABLE USER --
@@ -116,7 +116,7 @@ INSERT INTO tbl_user (
 VALUES (
   'Hiei', 'Hiei@test.com', 'img/avatar/default/default-avatar.png',
   'f55ff16f66f43360266b95db6f8fec01d76031054306ae4a4b380598f6cfd114', '1', NULL,
-  0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1
+  0, 10000001, current_timestamp, 10000001, current_timestamp, 1
 );
 
 -- INIT TABLE USER_ROLE --
@@ -124,14 +124,14 @@ INSERT INTO tbl_user_role (
   user_name, role_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES ('Hiei', 1, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (10000001, 1, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_user_role (
   user_name, role_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES ('Hiei', 2, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (10000001, 2, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
 INSERT INTO tbl_user_role (
   user_name, role_code,
   delete_flag, create_user_id, create_datetime,
   update_user_id, update_datetime, version
-) VALUES ('Hiei', 3, 0, 'Hiei', current_timestamp, 'Hiei', current_timestamp, 1);
+) VALUES (10000001, 3, 0, 10000001, current_timestamp, 10000001, current_timestamp, 1);
